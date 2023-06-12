857c152e8ece5e545d05fd04d5b1fc49de914eff
Lili Mkrtchyan <lm688@cornell.edu>
2023-06-12 06:22:39 -0400

    dynamic company badge page

--- pages/company-badges.js
+++ pages/company-badges.js
@@ -1,8 +1,12 @@
 import { MdClose } from 'react-icons/md';
 import React from 'react';
+import {useState} from 'react';
+
 
 export default function CompanyBadges() {
   const activePage = 'badges'; // Set the active page here
+
+  const [selectedItem, setSelectedItem] = useState(null);
 
   const badges = [
     { id: 1, company: 'Subway', title: 'Silver Membership', image: 'assets/subway-membership.png' },
@@ -12,6 +16,28 @@ export default function CompanyBadges() {
     { id: 5, company: 'Subway', title: 'Silver Membership', image: 'assets/subway-membership.png' },
     { id: 6, company: 'Subway', title: 'Silver Membership', image: 'assets/subway-membership.png' },
   ];
+
+  const handleItemClick = (badge) => {
+    console.log('Item clicked:', badge);
+    setSelectedItem(badge);
+  }
+
+  console.log('Selected Item:', selectedItem);
+
+  if(selectedItem){
+    return(
+      <>
+
+        <div>
+          <img className='badge-page-image' src={selectedItem.image} alt='badge' />
+          <p className='badge-page-company'>{selectedItem.company}</p>
+          <p className='badge-page-title'>{selectedItem.title}</p>
+        </div>
+
+        <div className="notif-separator"></div>
+      </>
+    );
+  }
 
   return (
     <>
@@ -41,7 +67,10 @@ export default function CompanyBadges() {
       <div className="company-badge-container">
         {badges.map(badge => (
           <div className="company-badge-item" key={badge.id}>
-            <img className="company-badge-image" src={badge.image} alt='badge' />
+            <img className="company-badge-image" src={badge.image} alt='badge' onClick={() => {
+                  console.log('Image clicked');
+                  handleItemClick(badge)
+                  }}/>
             <div className="company-badge-info-title">
               <div className='company-badge-info'>
                 <img className='company-badge-logo' src='assets/placeholder-image.jpg' alt="Company Logo" />
