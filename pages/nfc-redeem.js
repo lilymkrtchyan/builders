b2a7f938706a5d585e7d6497f2aa792a9feb7859
Lili Mkrtchyan <lm688@cornell.edu>
2023-06-19 10:04:32 -0400

    updated nfc-redeem w buttons

--- pages/nfc-redeem.js
+++ pages/nfc-redeem.js
@@ -1,10 +1,17 @@
 import React from "react";
 
 export default function NFCRedeem() {
+
+   const activePage = 'nfc';
+
     return(
         <>
 
         <div className='centered-container'>
+            <div className='redeem-options'>
+                <a href='/nfc-redeem' className={`nfc-button ${activePage === 'nfc' ? 'redeem-active' : ''}`}>NFC</a>
+                <a className={`qr-code-button ${activePage === 'qr-code' ? 'redeem-active' : ''}`}>QR Code</a>
+            </div>
             <div className='info-and-image'>
                 <p className='nfc-company-name'>Joe & The Juice</p>
                 <p className='nfc-discount'>10% Discount</p>
