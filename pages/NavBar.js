e8e32c8a9f6723202888ff0e53f3811b34429bae
Lili Mkrtchyan <lm688@cornell.edu>
2023-07-02 05:08:51 -0400

    updated nav bar style

--- pages/NavBar.js
+++ pages/NavBar.js
@@ -1,19 +1,23 @@
 export default function NavBar() {
-    return  (
+  return (
     <footer>
-        <nav className="footer-nav">
-          <ul>
-            <li>
-              <a href="/" >Rewards</a>
-            </li>
-            <li>
-              <a href="/badges">Badges</a>
-            </li>
-            <li>
-              <a href="/discover">Discover</a>
-            </li>
-          </ul>
-        </nav>
+      <div className="footer-content">
+        {/* other content in the footer */}
+      </div>
+
+      <nav className="footer-nav">
+        <ul>
+          <li>
+            <a href="/">Rewards</a>
+          </li>
+          <li>
+            <a href="/badges">Badges</a>
+          </li>
+          <li>
+            <a href="/discover">Discover</a>
+          </li>
+        </ul>
+      </nav>
     </footer>
-    );
+  );
 }
